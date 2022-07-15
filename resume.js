6124bda7c50213761021ef702e303813776bfd58
Shivusangam <109031337+Shivusangam@users.noreply.github.com>
2022-07-15 21:12:23 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,75 @@
+let my_resume={
+    "personal": {
+      "name": "shivanand",
+      "label": "mechanical Engineer",
+      "email": "shivusangam17@gmail.com",
+      "phone": "9964540033",
+      "location": {
+        "address": "belgaum",
+        "postalCode": "560001",
+        "city": "belgaum",
+        "state" : "Karnataka",
+        "country": "india"
+      },
+    
+      "profiles": [
+        {
+          "network": "Github",
+          "username": "shivanand",
+          "url": "https://github.com/shivusangam/"
+        }
+      ]
+    },
+  
+    "education": [
+      {
+        "institution": "MMEC",
+        "area": "Mechanical Engineeing",
+        "studyType": "BE",
+        "start": "2015",
+        "end": "2018",
+        "score": "65%"
+      }
+    ],
+    "skills": [
+      {
+      
+        "keywords": [
+            "cncprogram",
+            "ansys",
+          "HTML",
+          "CSS",
+          "JavaScript",
+          "React js"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "English",
+        "fluency": "advance"
+      },
+      {
+        "language": "Kannada",
+        "fluency": "Native speaker"
+      },
+      {
+        "language": "hindi",
+        "fluency": "advance"
+      },
+      {
+        "language": "marathi",
+        "fluency": "medium"
+      }
+    ]
+  }
+
+  console.log(my_resume)
+  
+// {personal: {…}, work: Array(1), education: Array(1), skills: Array(1), languages: Array(2)}
+// education: [{…}]
+// languages: (2) [{…}, {…}]
+// personal: {name: 'shruti', label: 'Software Engineer', email: 'shrumanniker@gmail.com', phone: '8951211056', location: {…}, …}
+// skills: [{…}]
+// work: [{…}]
+// [[Prototype]]: Object
